bebd008bb1f72ef88c86b13cd906a60fd7fb86b4
Saus <dsaus@cerner.com>
2018-09-04 11:17:44 -0500

    First check in

---
+++ src/main/java/com/cerner/cts/oss/rabbit/processor/Processor.java
@@ -0,0 +1,29 @@
+package com.cerner.cts.oss.rabbit.processor;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+
+import com.cerner.cts.oss.mass.ingest.contract.IngestionEvent;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author ds3161
+ *
+ */
+@Slf4j
+@Component
+@EnableBinding({ InputSink.class, OutputSource.class })
+public class Processor {
+	/**
+	 * @param message
+	 */
+	@StreamListener(InputSink.myInput)
+	@SendTo(OutputSource.myOutput)
+	public IngestionEvent receive(IngestionEvent event) {
+		log.trace("Received event {}", event);
+		return event;
+	}
+}
